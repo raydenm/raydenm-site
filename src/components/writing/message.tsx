fc11a649428bb91ba86eaf22a25437ff0489bd46
raydenm <raydenm928@gmail.com>
2024-08-09 09:37:39 +0800

    feat: add message send

--- src/components/writing/message.tsx
+++ src/components/writing/message.tsx
@@ -8,6 +8,9 @@ export function Message({ slug }: { slug: string }) {
 
   useEffect(() => {
     if (!scriptAdded.current) {
+      console.log(`https://raydenm.zeabur.app/api/increment-views?slug=${slug}`)
+
+      fetch(`https://raydenm.zeabur.app/api/increment-views?slug=${slug}`)
       const scriptElement = document.createElement('script')
       scriptElement.async = true
       scriptElement.crossOrigin = 'anonymous'
