c243e7a9cd60976e4fa08771740aae31be870288
raydenm <raydenm928@gmail.com>
2024-08-13 15:27:54 +0800

    feat: change page

--- src/components/bookmarks/bookmark-list.tsx
+++ src/components/bookmarks/bookmark-list.tsx
@@ -8,6 +8,21 @@ import { BookmarkCard } from '@/components/bookmarks/bookmark-card'
 import { getBookmarkItemsByPageIndex } from '@/app/actions'
 import { cn } from '@/lib/utils'
 
+export type BookmarkItemType = {
+  _id: number
+  link: string
+  title: string
+  excerpt: string
+  note: string
+  cover: string
+  domain: string
+}
+
+export type BookmarkItemsType = {
+  items: BookmarkItemType[]
+  result: true
+}
+
 type BookmarkListProps = {
   initialData: any
   id: number
@@ -22,9 +37,35 @@ export const BookmarkList = ({ initialData, id }: BookmarkListProps) => {
     if (!isReachingEnd && !isLoading) setPageIndex((prevPageIndex) => prevPageIndex + 1)
   }
 
+  const getBookmarkItems = async (id: number, pageIndex = 0) => {
+    try {
+      const response = await fetch(
+        `https://api.raindrop.io/rest/v1/raindrops/${id}?` +
+          new URLSearchParams({
+            page: String(pageIndex),
+            perpage: '30',
+            time: String(Date.now()),
+            sort: 'created'
+          }),
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_RAINDROP_ACCESS_TOKEN}`
+          }
+        }
+      )
+      const data: BookmarkItemsType = await response.json()
+      return data
+    } catch (error) {
+      console.info(error)
+      return null
+    }
+  }
+
   const fetchInfiniteData = useCallback(async () => {
     setIsLoading(true)
-    const newData = await getBookmarkItemsByPageIndex(id, pageIndex)
+    const newData = await getBookmarkItems(id, pageIndex)
     if (newData?.result) setData((prevData: any) => [...prevData, ...newData.items])
     setIsLoading(false)
   }, [id, pageIndex])
