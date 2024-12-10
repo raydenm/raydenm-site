1e6b5b46ca8cc0b01a40665bf0cb573cd84d65e5
raydenm <raydenm928@gmail.com>
2024-12-10 17:25:35 +0800

    feat: change color

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -41,7 +41,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 border border-muted bg-background p-8 shadow-lg duration-200 focus:outline-[none] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-[32px] md:w-full',
+        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 border border-muted bg-background p-8 shadow-lg duration-200 focus:outline-0 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-[32px] md:w-full',
         className
       )}
       {...props}
@@ -52,7 +52,7 @@ const DialogContent = React.forwardRef<
         className={cn(
           `absolute ${
             isPreview ? 'right-[-5px] top-[-5px]' : 'right-7 top-7'
-          }  rounded-sm ring-offset-background transition-opacity focus:outline-none  disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground`,
+          }  rounded-sm ring-offset-background transition-opacity focus:outline-0  disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground`,
           closeClassName
         )}
       >
