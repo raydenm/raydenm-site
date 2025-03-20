d8436350ac43c49778ac5bc00e5a881475f798e1
raydenm <raydenm928@gmail.com>
2025-03-20 18:11:38 +0800

    feat: update views

--- src/components/writing/message.tsx
+++ src/components/writing/message.tsx
@@ -4,6 +4,20 @@ import Giscus from '@giscus/react'
 import { useEffect } from 'react'
 
 import useRealTheme from '@/hooks/useRealTheme'
+import supabase from '@/services/supabase/public'
+
+const updateViewCount = async (slug: string) => {
+  if (!slug) return
+  const { data: record } = await supabase.from('view_data').select('slug, view_count').eq('slug', slug).single()
+
+  if (!record) {
+    await supabase.from('view_data').insert({ slug, view_count: 1 }).select().single()
+  } else {
+    const newViewCount = (record?.view_count || 0) + 1
+    await supabase.from('view_data').update({ view_count: newViewCount }).eq('slug', slug).select().single()
+  }
+}
+
 export function Message({ slug }: { slug: string }) {
   const repo = process.env.NEXT_PUBLIC_REPO || ''
   const repoId = process.env.NEXT_PUBLIC_REPO_ID || ''
@@ -12,7 +26,7 @@ export function Message({ slug }: { slug: string }) {
 
   // 记录访问量
   useEffect(() => {
-    fetch(`/api/increment-views?slug=${slug}`)
+    updateViewCount(slug)
   }, [slug])
 
   const realTheme = useRealTheme()
